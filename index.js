8e0d395c6c3670c0494e251cc46ca623eb6b2574
kbarkins <kyle@kylebarkins.com>
2025-03-24 19:51:09 -0400

    added the form and post request. pulled in street address as it was missing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+// Middleware
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
@@ -42,19 +43,42 @@ app.get('/', async (req, res) => {
 
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// Route for update custom object form
-app.get('/update-cobj', (req, res) => {
-    res.render('updates', { 
-        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
-    });
-});
-
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+// * Code for Route 2 & 3 is below
+
+// Routes for update custom object form
+app.route('/update-cobj')
+    .get((req, res) => {
+        res.render('updates', { 
+            title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+        });
+    })
+    .post(async (req, res) => {
+        console.log('Received form data:', req.body); // Debug log
+        
+        const homesURL = `https://api.hubspot.com/crm/v3/objects/${CUSTOM_OBJECT_ID}`;
+        const headers = {
+            Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+            'Content-Type': 'application/json'
+        }
+
+        const properties = {
+            name: req.body.name,
+            street_address: req.body.street_address,
+            state: req.body.state,
+            short_description: req.body.short_description
+        };
+
+        try {
+            const response = await axios.post(homesURL, { properties }, { headers });
+            console.log('HubSpot API Response:', response.data); // Debug log
+            res.redirect('/');
+        } catch (error) {
+            console.error('Error creating home:', error.response?.data || error.message);
+            res.status(500).send('Error creating home: ' + (error.response?.data?.message || error.message));
+        }
+    });
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
